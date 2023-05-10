e6be1f9a541c39b3fb55502fbd16cee14e0c90a7
surf08 <105436365+surf08@users.noreply.github.com>
2023-05-10 23:46:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <Windows.h>
 #include <Psapi.h>
 
-DWORD FindPattern(const char *module, const char *pattern, const char *mask)
+uintptr_t FindPattern(const char *module, const char *pattern, const char *mask)
 {
 	MODULEINFO mInfo = { 0 };
 	GetModuleInformation(GetCurrentProcess(), GetModuleHandle(module), &mInfo, sizeof(MODULEINFO));
-	DWORD base = (DWORD)mInfo.lpBaseOfDll;
-	DWORD size = (DWORD)mInfo.SizeOfImage;
-	DWORD patternLength = (DWORD)strlen(pattern);
+	uintptr_t base = (uintptr_t)mInfo.lpBaseOfDll;
+	uintptr_t size = (uintptr_t)mInfo.SizeOfImage;
+	uintptr_t patternLength = (uintptr_t)strlen(pattern);
 	bool found;
-	for (DWORD i = 0; i < size - patternLength; i++)
+	for (uintptr_t i = 0; i < size - patternLength; i++)
 	{
 		found = true;
-		for (DWORD j = 0; j < patternLength; j++)
+		for (uintptr_t j = 0; j < patternLength; j++)
 			found &= mask[j] == '?' || pattern[j] == *(char *)(base + i + j);
 		if (found)
 			return base + i;
@@ -24,7 +24,7 @@ bool VMTHook(void *instance, void **oldfunc, void *newfunc, uintptr_t offset)
 {
 	uintptr_t vtable = *((uintptr_t *)instance) + sizeof(uintptr_t) * offset;
 	*oldfunc = (void *)(*((uintptr_t *)vtable));
-	DWORD oldProtect;
+	uintptr_t oldProtect;
 	if (VirtualProtect((LPVOID)vtable, sizeof(vtable), PAGE_EXECUTE_READWRITE, &oldProtect))
 	{
 		*((uintptr_t *)vtable) = (uintptr_t)newfunc;
